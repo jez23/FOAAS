d86b810ae5f8bdd63d5cd005de5d06875b5d3a07
jez23 <jblackmoreuk@hotmail.co.uk>
2017-09-03 16:57:35 +0100

    added basic Bootstrap styling

--- controllers/foaasControllers.js
+++ controllers/foaasControllers.js
@@ -1,20 +1,56 @@
 function foaasControllers(app){
-  app.get('/smell/:from/:to', function(req, res) {
+  
+  app.get('/momma/:from/:to', function(req, res) {
       res.format({
         html: function () {
           res.render('home', {
             "message": "Hey " + req.params.to + " Yo momma is so fat, I took a picture of her last Christmas and it's still printing.",
-            "from": req.params.from
+            "from": "Love " + req.params.from
           }
         )},
       default: function () {
           res.json({  
             "message": "Hey " + req.params.to + " Yo momma is so fat, I took a picture of her last Christmas and it's still printing.",
-            "from": req.params.from 
+            "from": "Love " + req.params.from 
           })
       }
       })
   })
+
+    app.get('/marble/:from/:to', function(req, res) {
+      res.format({
+        html: function () {
+          res.render('home', {
+            "message": "Hey " + req.params.to + " you're as sharp as a marble.",
+            "from": "Love " + req.params.from
+          }
+        )},
+      default: function () {
+          res.json({  
+            "message": "Hey " + req.params.to + " you're as sharp as a marble.",
+            "from": "Love " + req.params.from 
+          })
+      }
+      })
+  })
+
+      app.get('/poopoo/:from/:to', function(req, res) {
+      res.format({
+        html: function () {
+          res.render('home', {
+            "message": req.params.to + " you're a poo poo head.",
+            "from": "Love " + req.params.from
+          }
+        )},
+      default: function () {
+          res.json({  
+            "message": req.params.to + " you're a poo poo head.",
+            "from": "Love " + req.params.from 
+          })
+      }
+      })
+  })
+
 }
 
 
